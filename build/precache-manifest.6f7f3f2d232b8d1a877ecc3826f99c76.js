509d63cb72621bd31445b0373b28ccf0ae4e62ae
aliciajpan <37771924+aliciajpan@users.noreply.github.com>
2020-09-03 16:56:18 -0400

    intial
    depart from collaborate

---
+++ build/precache-manifest.6f7f3f2d232b8d1a877ecc3826f99c76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e075ef4def74d66dd5b84e0676f256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c57320134b8c1c3568a7",
+    "url": "/static/css/main.3220dc00.chunk.css"
+  },
+  {
+    "revision": "268048b49648b863effc",
+    "url": "/static/js/2.69dcbedf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69dcbedf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c57320134b8c1c3568a7",
+    "url": "/static/js/main.c60d97a8.chunk.js"
+  },
+  {
+    "revision": "c9a422e4a913583e4ca6",
+    "url": "/static/js/runtime-main.f4869d9f.js"
+  }
+]);
